f1552c20b432a73bdd2b08291782b92e47e0dc3b
Echo5Mike <1Raul.Marquez@gmail.com>
2021-07-30 18:50:13 -0700

    done with all code challges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,7 +1,6 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
-const { expect } = require("@jest/globals")
-
+const { array } = require("yargs")
 
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
@@ -33,26 +32,26 @@ const { expect } = require("@jest/globals")
 // maybe a shuffle method? math random
 
 ////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// describe("function", () => {
-//     test("a function that takes in an array, removes the first item from the array and shuffles the remaining content", () => {
-//       expect(arrayIn(["purple", "blue", "green", "yellow", "pink"])).toContain("blue", "green", "yellow", "pink")
-//     expect(arrayIn(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"])).toContain("indigo", "periwinkle", "ochre", "aquamarine", "saffron")
-//     })
-// })
-// // I had to look for the .toContain and it only works if you remove the [] from the array
-// var colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// const arrayIn = (array) =>{
-//     oneLess = []
-//     for (let i = 0; i < array.length; i++) {
-//        oneLess = array.slice(1)
-//     }
-//     cupidShuffle = oneLess.sort(() => Math.random() - 0.5) // this math.random I had to look up for the syntex
-//     return cupidShuffle
-// } 
+describe("function", () => {
+    test("a function that takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+      expect(arrayIn(["purple", "blue", "green", "yellow", "pink"])).toContain("blue", "green", "yellow", "pink")
+    expect(arrayIn(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"])).toContain("indigo", "periwinkle", "ochre", "aquamarine", "saffron")
+    })
+})
+// I had to look for the .toContain and it only works if you remove the [] from the array
+var colors1 = ["purple", "blue", "green", "yellow", "pink"]
+var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+const arrayIn = (array) =>{
+    oneLess = []
+    for (let i = 0; i < array.length; i++) {
+       oneLess = array.slice(1)
+    }
+    cupidShuffle = oneLess.sort(() => Math.random() - 0.5) // this math.random I had to look up for the syntex
+    return cupidShuffle
+} 
 
-// console.log(arrayIn(colors1))
-// console.log(arrayIn(colors2))
+console.log(arrayIn(colors1))
+console.log(arrayIn(colors2))
 ////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
@@ -70,44 +69,63 @@ const { expect } = require("@jest/globals")
 // // fn will ONLY ONLY ONLY ONLY>>>>> return 2 numbers min and MAX
 // // might need an empty array.
 
-// describe("an Array of numbers", () => {
-//     it("only the smallest number and largest number", () => {
-//       expect(numsArray([3, 56, 90, -8, 0, 23, 6])).toEqual([-8, 90])
-//       expect(numsArray([109, 5, 9, 67, 8, 24])).toEqual([5, 109])
-//     })
-// })
-// const numsArray = (array) =>{
-//     sortedNums = array.sort((a, b) => a-b)
-//     lilNum = sortedNums.shift()// so i managed to isloted each value that I wanted, .concat is not working
-//     bigNum = sortedNums.pop()
-//     answerArray =[lilNum,bigNum] //after a very long while, I thought will maybe its as easy as calling my new var's into an array and yes, yes it is. 
-//     // console.log(bigNum)
-//     // console.log(lilNum)
-
-//     return answerArray
-// }
-// var nums1 = [3, 56, 90, -8, 0, 23, 6]
-// console.log(numsArray(nums1))
+describe("an Array of numbers", () => {
+    it("only the smallest number and largest number", () => {
+      expect(numsArray([3, 56, 90, -8, 0, 23, 6])).toEqual([-8, 90])
+      expect(numsArray([109, 5, 9, 67, 8, 24])).toEqual([5, 109])
+    })
+})
+const numsArray = (array) =>{
+    sortedNums = array.sort((a, b) => a-b)
+    lilNum = sortedNums.shift()// so i managed to isloted each value that I wanted, .concat is not working
+    bigNum = sortedNums.pop()
+    answerArray =[lilNum,bigNum] //after a very long while, I thought will maybe its as easy as calling my new var's into an array and yes, yes it is. 
+    // console.log(bigNum)
+    // console.log(lilNum)
+    return answerArray
+}
+var nums1 = [3, 56, 90, -8, 0, 23, 6]
+console.log(numsArray(nums1))
 // // Expected output: [-8, 90]
-// var nums2 = [109, 5, 9, 67, 8, 24]
-// console.log(numsArray(nums2))
+var nums2 = [109, 5, 9, 67, 8, 24]
+console.log(numsArray(nums2))
 // // Expected output: [5, 109]
 
 
-
-
-
-
-
-
-
+////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
-
 // a) Create a test with an expect statement using the variables provided.
 
-// var testArray1 = [3, 7, 10, 5, 4, 3, 3]
-// var testArray2 = [7, 8, 2, 3, 1, 5, 4]
+//create a test and let it fail
+//function
+    // will take in testArray1,2
+    // concat two arrays then sort?
+    // we sort then concat
+//duplicates   
+    //maybe a for loop simulir to febanchi 
+    //.filiter
+    // going to need any empty array to hold new numbers
+
+
+describe("function that test two arrays", () => {
+    test("takes in two arrays of numbers, returns ONE array with no duplicates", () => {
+      expect(twins([3, 7, 10, 5, 4, 3, 3],[7, 8, 2, 3, 1, 5, 4])).toContain(3, 7, 10, 5, 4, 8, 2, 1)
+    })
+})
+const twins = (array1, array2)=>{
+    newArray = (array2.concat(array1)).sort((a,b) => a-b)
+    noDubs = []
+    for(let i=0; i<newArray.length; i++){
+        if(newArray[i] !== newArray[i+1])// I really don't know how I did it but I did
+        noDubs.push(newArray[i])
+    }
+    return noDubs//there are no duplictes in the array of numbers but they are sorted from lwo to high
+}
+
+var testArray1 = [3, 7, 10, 5, 4, 3, 3]
+var testArray2 = [7, 8, 2, 3, 1, 5, 4]
+console.log(twins(testArray1,testArray2))
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 
